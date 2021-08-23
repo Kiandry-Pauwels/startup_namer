f72f5538eec0e6b6f2a785dded5995abc18587bd
Kiandry-Pauwels <kiandry@skynet.be>
2021-08-23 17:21:33 +0200

    Imported test package andstateful class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,9 +18,24 @@ class MyApp extends StatelessWidget {
           title: const Text('Forget Me Not'),
         ),
         body: const Center(
-          child: Text('This is a shopping list app to be'),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
